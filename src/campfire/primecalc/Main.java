6f1a402757fc2c28c10082d2c9f64265b8c9d0ba
Mirko Cegledi <mirko.cegledi@osudio.com>
2015-02-09 16:23:59 +0100

    renamed variables, replaced input-method with a more common one, replaced "answersPerQuestion" constant with a variable value asked for at the prompt.

--- src/campfire/primecalc/Main.java
+++ src/campfire/primecalc/Main.java
@@ -4,41 +4,28 @@ import java.util.Scanner;
 
 public class Main {
 
-	private final static int DEFAULT_SIZE = 4;
-
 	public static void main(final String[] args) {
 
 		Scanner in = new Scanner(System.in);
 		Main main = new Main();
 
-		// enter number (of questions)
-		int numberOfQ = main.fetchNumber(in);
-		int numberOfTurns = main.fetchTurns(in);
+		int questionsInQuiz = main.fetchNumber(in, "Please enter number of questions: ");
+		int anwersPerQuestion = main.fetchNumber(in, "Enter number of answers per question: ");
+		int calculationSets = main.fetchNumber(in, "Please enter number of calculation sets: ");
 
 		in.close();
 
-		for (int i = 0; i < numberOfTurns; i++) {
-			PrimeResultSet foo = new PrimeResultSet(numberOfQ, DEFAULT_SIZE);
-			System.out.println(foo.toString());
+		for (int i = 0; i < calculationSets; i++) {
+			PrimeResultSet results = new PrimeResultSet(questionsInQuiz, anwersPerQuestion);
+			System.out.println(results.toString());
 			System.out.println("-------------------");
 		}
 	}
 
-	private int fetchNumber(final Scanner in) {
-
-		System.out.print("Please enter number of questions: ");
+	private int fetchNumber(final Scanner in, final String message) {
+		System.out.print(message);
 		int number = Integer.valueOf(in.nextLine());
 
 		return number;
 	}
-
-	private int fetchTurns(final Scanner in) {
-
-		int number = 1;
-		System.out.print("Please enter number of turns: ");
-		number = Integer.valueOf(in.nextLine());
-
-		return number;
-	}
-
 }
